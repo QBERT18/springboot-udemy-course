a98b911dc902cb89ce7f397b84d3f9cfed6a37a9
Q-bert <valeh.mustafayev.98@gmail.com>
2023-04-25 20:02:52 +0200

    first commit

---
+++ src/main/java/com/example/springboot/udemy/couse/springbootudemycourse/SpringbootUdemyCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.udemy.couse.springbootudemycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUdemyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUdemyCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/udemy/couse/springbootudemycourse/SpringbootUdemyCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.udemy.couse.springbootudemycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUdemyCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
